ab5b752fc9d8f4fc11652f43396ca25cfda4e0f5
Chris Siku <chrissiku5@gmail.com>
2022-12-17 12:53:30 +0200

    Configure conditional fallback

--- sw.js
+++ sw.js
@@ -56,6 +56,10 @@ self.addEventListener("fetch", (evt) => {
           })
         );
       })
-      .catch(() => caches.match("/pages/fallback.html"))
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/pages/fallback.html");
+        }
+      })
   );
 });
